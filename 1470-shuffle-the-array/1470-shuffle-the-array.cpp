ed3064aabd575e27313c17e4b2d436fc898b9bc1
gaurav446446-hue <gaurav446446@gmail.com>
2026-07-02 21:01:14 +0530

    Time: 8 ms (17.37%), Space: 13.6 MB (17.86%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> temp(2*n);
+        for(int i=0; i<n; i++)
+        {
+            temp[2*i] = nums[i];
+            temp[2*i+1] = nums[i+n];
+        }
+        nums = temp;
+        return nums;
+    }
+};
